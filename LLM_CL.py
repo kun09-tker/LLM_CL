e582ee84c15e1fb262d24e48642060b8ccf22bfa
kun09-tker <ltanloc97o@gmail.com>
2025-05-10 12:17:50 +0700

    .

--- LLM_CL.py
+++ LLM_CL.py
@@ -185,7 +185,7 @@ class DomainPositioning:
         return best_domain
 
     def get_hidden(self, model, input_ids, adapter):
-        outputs = self.tokenizer(**input_ids).to(model.device)
+        outputs = model(**input_ids)
         hidden_states = outputs.pooler_output
         lora_output = adapter(hidden_states)
         return lora_output
