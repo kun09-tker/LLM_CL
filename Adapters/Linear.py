aef61a172a9bffd4397a4aaca8db07f6d67171d8
kun09-tker <ltanloc97o@gmail.com>
2025-05-14 00:37:51 +0700

    .

--- Adapters/Linear.py
+++ Adapters/Linear.py
@@ -1,9 +1,15 @@
+import torch
 import torch.nn as nn
 
 class LinearWrapper(nn.Module):
     def __init__(self, hidden_size):
         super().__init__()
         self.linear = nn.Linear(hidden_size, hidden_size)
+        self.device = torch.device("cpu")
 
     def forward(self, x):
         return self.linear(x)
+
+    def to(self, device):
+        self.device = torch.device(device)
+        return super().to(device)
