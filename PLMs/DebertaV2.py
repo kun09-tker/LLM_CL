b3a69107327846d1f8fea89fcfa3a00150398fe6
kun09-tker <ltanloc97o@gmail.com>
2025-06-28 23:46:46 +0700

    .

--- PLMs/DebertaV2.py
+++ PLMs/DebertaV2.py
@@ -88,12 +88,14 @@ class MyDebertaV2ForSequenceClassification(DebertaV2ForSequenceClassification):
             name: nn.Sequential(
               nn.Dropout(0.5),
               nn.Linear(self.config.hidden_size, 128),
+              ACT2FN[self.config.pooler_hidden_act],
               nn.Linear(128, self.config.num_labels)
             )
             for name in domain_names})
         self.classifier_share = nn.Sequential(
             nn.Dropout(0.5),
             nn.Linear(self.config.hidden_size, 128),
+            ACT2FN[self.config.pooler_hidden_act],
             nn.Linear(128, self.config.num_labels)
         )
         self.post_init()
