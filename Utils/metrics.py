c03dcf203c92851aa9464e758d504810f417b8fe
kun09-tker <ltanloc97o@gmail.com>
2025-06-30 23:58:57 +0700

    .

--- Utils/metrics.py
+++ Utils/metrics.py
@@ -5,8 +5,8 @@ import numpy as np
 from sklearn.metrics import accuracy_score, f1_score
 
 def classifier_metrics(preds, labels, average="macro", device='cpu'):
-    preds = torch.cat(preds).to(device)
-    labels = torch.cat(labels).to(device)
+    preds = preds.to(device)
+    labels = labels.to(device)
     preds = preds.cpu().numpy()
     labels = labels.cpu().numpy()
     acc = accuracy_score(labels, preds)
