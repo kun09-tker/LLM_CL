b6b7f03b9ec441a573776db9192bbbef2b62054a
kun09-tker <ltanloc97o@gmail.com>
2025-12-02 21:22:23 +0700

    add attention_mask when get embedding

--- PLMs/DebertaV2.py
+++ PLMs/DebertaV2.py
@@ -180,7 +180,7 @@ class MyDebertaV2ForSequenceClassification(DebertaV2ForSequenceClassification):
         sequence_output = outputs.last_hidden_state if return_dict else outputs[0]
 
         if return_emb:
-            return sequence_output
+            return sequence_output, attention_mask
         # print(outputs)
         # reshape = sequence_output.squeeze(0)
 
